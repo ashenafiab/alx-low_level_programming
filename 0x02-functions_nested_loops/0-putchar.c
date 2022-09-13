347852b6fd45076c3efddc7679c3089caeda2232
Ashenafi <ashenafiabebaw02@gmail.com>
2022-09-13 11:02:06 +0300

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "Ashenafi.h"
+
+/*
+ * Description: main - program that prints Ashenafi.
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	char str_name[8] = "Ashenafi";
+
+	while (n < 8)
+	{
+		_putchar(str_name[n]);
+		n += 1;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
